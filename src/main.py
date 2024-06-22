7a1124a8a0eb20c65b14c77905cf9cafc22760b7
Bruno Rosendo <brunomrosendo@hotmail.com>
2024-06-22 19:05:10 +0100

    Fixed QPU usage not stored when using hybrid CQM sampler

--- src/main.py
+++ src/main.py
@@ -7,6 +7,35 @@ from src.solver.qubo.DWaveSolver import DWaveSolver
 from src.solver.qubo.QiskitSolver import QiskitSolver, get_backend_sampler
 
 if __name__ == "__main__":
-    load_dotenv()
-    result = VRPSolution.from_json("300+302-3v")
+    cvrp = DWaveSolver(
+        1,
+        None,
+        [
+            (456, 320),
+            (228, 0),
+            (912, 0),
+            # (0, 80),
+            # (114, 80),
+            # (570, 160),
+            # (798, 160),
+            # (342, 240),
+            # (684, 240),
+            # (570, 400),
+            # (912, 400),
+            # (114, 480),
+            # (228, 480),
+            # (342, 560),
+            # (684, 560),
+            # (0, 640),
+            # (798, 640),
+        ],
+        [
+            (0, 3, 5),
+            (2, 1, 6),
+        ],
+        False,
+        # sampler=LeapHybridCQMSampler(),
+    )
+    result = cvrp.solve()
+    # result = VRPSolution.from_json("300+302-3v")
     result.display()
