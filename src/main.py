f2dad7a26c85270efee5fb3f4d6f69ce574613b6
Bruno Rosendo <brunomrosendo@hotmail.com>
2024-08-28 02:15:27 +0100

    Refactored data inputs

--- src/main.py
+++ src/main.py
@@ -7,7 +7,8 @@ if __name__ == "__main__":
     load_dotenv()
 
     model = CVRP(1, [(46, 32), (20, 32), (71, 32), (46, 60), (46, 4)], 5, [1] * 5)
+    m2 = RPP(1, [(5, 5), (15, 10)], None, [])
 
-    solver = ClassicSolver(model)
+    solver = ClassicSolver(m2)
     solution = solver.solve()
     solution.display()
